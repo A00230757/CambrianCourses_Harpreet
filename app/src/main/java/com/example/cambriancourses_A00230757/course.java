b2689a70aaee878d381bb1bb49ab7e07caaa7b34
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-07-17 19:08:23 -0400

    in add courses earlier there was no course code and professor.
     now i made few changes in  this activity and first of all departments are visible in spinner and in second spinner professors in that department are shown. Admin can choose any department and professor under that departmnet to add course. So in this way new course with its code , name , description is added and also professor is assigned to the course.
    
     Files changed course.java (few columns added)
     activity_add_courses.xml (spinner for professor , course code edittext added)
     AddCoursesActivity.java (professor for selected department fetched automatically  to assign to course)

--- app/src/main/java/com/example/cambriancourses_A00230757/course.java
+++ app/src/main/java/com/example/cambriancourses_A00230757/course.java
@@ -1,20 +1,24 @@
 package com.example.cambriancourses_A00230757;
 
 public class course {
+    public String coursecode;
     public String name;
     public String description;
     public String path;
     public String under_dept;
+    public String professor;
     course(){
         name="";
         description = "";
     }
-    public course(String name,String description,String path , String under_dept)
+    public course(String coursecode ,String name,String description,String path , String under_dept, String professor)
     {
+        this.coursecode=coursecode;
         this.name=name;
         this.description = description;
         this.path = path;
         this.under_dept=under_dept;
+        this.professor=professor;
     }
 
 
