6e2ef032cdcafe958fa9a8a8631f13d7842d0a3b
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-08-16 17:17:00 -0400

    Student View Selected Courses and their detail.
    students can now see their selected courses in a separate actvity . A list is generated from the firebase database of a particular student who is logged in and when student click on detail of a particular course then a detail activity opens to show all the details of selected course.

---
+++ app/src/main/java/com/example/cambriancourses_A00230757/SelectedCourseDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.cambriancourses_A00230757;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectedCourseDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_course_detail);
+    }
+}
---
+++ app/src/main/java/com/example/cambriancourses_A00230757/selectedcourse.java
@@ -0,0 +1,14 @@
+package com.example.cambriancourses_A00230757;
+
+public class selectedcourse{
+    public String coursecode;
+    public String studentid;
+
+    selectedcourse() {
+    }
+
+    public selectedcourse(String coursecode, String studentid) {
+        this.coursecode = coursecode;
+        this.studentid = studentid;
+    }
+}
